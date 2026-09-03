e6db83790bea1a25b9e62fd038c06a09b98ad57f
TheBjoRedCraft <thebjoredcraft@gmail.com>
2026-09-03 21:36:20 +0200

    feat: update map loading mechanism to use AnvilLoader for improved instance management

--- surf-gecko-server/src/main/kotlin/dev/slne/surf/gecko/server/gecko/map/GeckoMapManager.kt
+++ surf-gecko-server/src/main/kotlin/dev/slne/surf/gecko/server/gecko/map/GeckoMapManager.kt
@@ -1,19 +1,21 @@
 package dev.slne.surf.gecko.server.gecko.map
 
+import dev.slne.surf.api.core.messages.adventure.key
 import kotlinx.coroutines.coroutineScope
-import net.hollowcube.polar.PolarLoader
 import net.minestom.server.MinecraftServer
 import net.minestom.server.instance.InstanceContainer
+import net.minestom.server.instance.anvil.AnvilLoader
 import kotlin.io.path.Path
 
 object GeckoMapManager {
     suspend fun prepareMap(map: GeckoMap): InstanceContainer = coroutineScope {
         val map = MinecraftServer.getInstanceManager()
-            .createInstanceContainer(PolarLoader(Path("maps/${map.mapName}")))
-
-        map.chunks.forEach {
-            map.loadChunk(it.toPosition())
-        }
+            .createInstanceContainer(
+                AnvilLoader(
+                    Path("maps/${map.mapName}"),
+                    key("minecraft:overworld")
+                )
+            )
 
         map
     }
